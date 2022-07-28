9e4f63c8e5982b4b953fc5039b27bef8bf579edc
Daitarus <chajkovskij_99@mail.ru>
2022-07-28 18:45:40 +0300

    tcp conection server+client

---
+++ Client/Client/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+namespace Client
+{
+    internal class Program
+    {
+        static int Main(string[] args)
+        {
+            //проверка параметров
+            if (args.Length != 5)
+            {
+                Console.WriteLine("Ошибка: Не верное колличество параметров !!!");
+                return -1;
+            }
+            //преобразование параметров в нужный тип
+            IPAddress ip = IPAddress.Parse("127.0.0.1"); int port_Tcp = 0, port_Udp=0, time=0; string file_temp;
+            try
+            {
+                ip = IPAddress.Parse(args[0]);
+                port_Tcp = Convert.ToInt32(args[1]);
+                port_Udp = Convert.ToInt32(args[2]);
+                file_temp = args[3];
+                time = Convert.ToInt32(args[4]);
+            }
+            catch
+            {
+                Console.WriteLine("Ошибка: Параметры введены неверно !!!");
+                return -1;
+            }
+            Console.WriteLine($"{ip} {port_Tcp} {port_Udp} {file_temp} {time}");
+            //подключение TCP
+            IPEndPoint ipPoint = new IPEndPoint(ip, port_Tcp);
+            Socket socket_Tcp = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            try
+            {
+                socket_Tcp.Connect(ipPoint);
+                Console.WriteLine("Подключено !!!");
+            }
+            catch
+            {
+                Console.WriteLine("Ошибка подключения !!!");
+            }
+            return 0;
+        }
+    }
+}
---
+++ Server/Server/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+namespace Server
+{
+    internal class Program
+    {
+        static int Main(string[] args)
+        {
+            //проверка параметров
+            if(args.Length != 3)
+            {
+                Console.WriteLine("Ошибка: Не верное колличество параметров !!!");
+                return -1;
+            }
+            //преобразование параметров в нужный тип
+            string temp; IPAddress ip = IPAddress.Parse("127.0.0.1"); int port = 0;
+            try
+            {
+                ip = IPAddress.Parse(args[0]);
+                port = Convert.ToInt32(args[1]);
+                temp = args[2];
+            }
+            catch
+            {
+                Console.WriteLine("Ошибка: Параметры введены неверно !!!");
+                return -1;
+            }
+            //создание сокета и прослушивание на подключение
+            Socket socket_Tcp = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint ipPoint = new IPEndPoint(ip, port);
+            try
+            {
+                socket_Tcp.Bind(ipPoint);
+                socket_Tcp.Listen(1);
+                Console.WriteLine("Ожидание поключения ...");
+                socket_Tcp.Accept();
+            }
+            catch
+            {
+                Console.WriteLine("Ошибка соединения !!!");
+                return -1;
+            }
+            Console.WriteLine("Новое подключение !!!");
+            
+            Console.ReadLine();
+            socket_Tcp.Shutdown(SocketShutdown.Both);
+            socket_Tcp.Close();
+            return 0;
+        }
+    }
+}
